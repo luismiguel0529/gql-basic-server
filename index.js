b5ae2489c442f882b7ec5878ab28869c3f603407
Luis Miguel Rodríguez Herrera - Ceiba Software <miguel.rodriguez@ceiba.com.co>
2021-10-28 10:39:43 -0500

    basic gql

---
+++ index.js
@@ -0,0 +1,62 @@
+import { ApolloServer, gql } from "apollo-server"
+
+const persons = [
+    {
+        name: "Miguel",
+        phone: "300320202",
+        street: "Calle frontend",
+        city: "Medellin",
+        id: "2ddd333dd-3333ss555dfd-3e34343et4"
+    },
+    {
+        name: "Carlos",
+        phone: "2223333",
+        street: "Calle backend",
+        city: "Bello",
+        id: "34343434-dssdsdsd-hhnhnhn"
+    },
+    {
+        name: "Mario",
+        street: "Calle Fullstack",
+        city: "Itagui",
+        id: "vbvbvbv-hgghghr444-5t55t5gdsdf"
+    }
+]
+
+const typeDefinitions = gql`
+    type Person {
+        name: String!
+        phone: String
+        street: String!
+        city: String!
+        id: ID!
+    }
+    
+
+    type Query {
+        personCount: Int!
+        allPersons: [Person]!
+        findPerson(name: String!): Person
+    }
+`
+
+const resolvers = {
+    Query: {
+        personCount: () => persons.length,
+        allPersons: () => persons,
+        findPerson: (root, args) => {
+            const {name} = args
+            return persons.find(persons => 
+                persons.name == name )
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs: typeDefinitions,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`)
+}) 
